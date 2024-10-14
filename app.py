126b4f648683d9e16c430833555224307ed7365e
anduswnmwek <91423581+anduswnmwek@users.noreply.github.com>
2024-10-14 19:20:51 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_input_image_info(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()    ## read the file into bytes
+        image_parts = [
+            {
+                "mime_type":uploaded_file.type,   ## get the type of file
+                "data":bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+st.set_page_config(page_title="Food Calorie")
+
+st.header("Food Calories Web Application")
+
+input = st.text_input("Ask any question:",key="input")
+uploaded_file = st.file_uploader("Choose an Invoice image..",type=["jpg","jpeg","png"])
+submit = st.button("Submit")
+
+input_prompt = """You are an nutrition expert where you need to see the food items 
+present in the input image and answer the question being asked, if the user don't ask
+any question then calculate the total calories, also provide the details 
+of each food items with calorie intake in below format:
+
+1. Item 1 - no of calories
+2. Item 2 - no of calories
+------
+------
+"""
+
+def get_gemini_model_response(input_prompt,image,question):
+    model = genai.GenerativeModel("gemini-pro-vision")
+    response = model.generate_content([input_prompt,image,question])
+    return response.text
+
+if submit:
+    image_data = get_input_image_info(uploaded_file)
+    model_response = get_gemini_model_response(input_prompt,image_data[0],input)
+    st.write(model_response)
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image,caption="Uploaded Invoice",use_column_width=True)
+
